445d5a53c3d31efaff7e5cafb67754f3cdf428a6
cwverica <cwverica@gmail.com>
2021-06-14 04:48:45 -0700

    I forgot to save before committing

--- math_routing.js
+++ math_routing.js
@@ -149,9 +149,9 @@ app.use(function (err, req, res, next) {
     });
 });
 
-// app.listen(3000, function () {
-//     console.log('Server is listening on port 3000');
-// });
+app.listen(3000, function () {
+    console.log('Server is listening on port 3000');
+});
 
 module.exports = {
     meanCalc,
